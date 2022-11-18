4c1eed8e5ff7480c44a142aac07e75d63ad36d7c
robiulxasan <robiul15-2516@diu.edu.bd>
2022-11-18 23:19:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users,setUser]= useState([]);
+  
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data=> setUser(data))
+
+   },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>I am a React developer</h2>
+        <UserCounter></UserCounter>
+        {
+          users.map(user=><Users name={user.name} key={user.id} email={user.email}></Users>)
+        }
+        
       </header>
     </div>
   );
+}
+
+function Users(props) {
+  const userStyle = {
+    border: "2px solid purple",
+    borderRadius: "10px",
+    width: '600px',
+    padding: "10px",
+    margin: "10px",
+  };
+  return (
+    <div style={userStyle}>
+      <h2>Name: {props.name} </h2>
+      <p style={{color:'yellow'}}>email: {props.email} </p>
+    </div>
+  );
+}
+
+function UserCounter(){
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3>Total User: {count} </h3>
+      <button onClick={()=>setCount(count + 1)}>Add User</button>
+      <button onClick={()=>setCount(count - 1)}>Remove User</button>
+    </div>
+  )
 }
 
 export default App;
